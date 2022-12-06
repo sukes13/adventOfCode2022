374e12ebee4b673c840b1bd5b77069bd29ab262a
sukes13 <sir_jelle_wouters@hotmail.com>
2022-12-06 12:16:20 +0100

    Day 6 - cleanup

--- src/main/kotlin/be/fgov/sfpd/kata/aoc22/day6/TuningTrouble.kt
+++ src/main/kotlin/be/fgov/sfpd/kata/aoc22/day6/TuningTrouble.kt
@@ -4,12 +4,11 @@ fun part1(input: String) = input.firstMarker(4)
 
 fun part2(input: String) = input.firstMarker(14)
 
-
-fun String.firstMarker(markerLength: Int) =
-        this.windowed(markerLength, 1)
+fun String.firstMarker(markerSize: Int) =
+        this.windowed(markerSize, 1)
             .mapIndexed { index, chunk ->
                 when (chunk.toList().distinct().size) {
-                    markerLength -> index + markerLength
+                    markerSize -> index + markerSize
                     else -> 0
                 }
             }.first { it != 0 }
