61fea677fef42f3db24d22854406bec4a4b656e2
sukes13 <sir_jelle_wouters@hotmail.com>
2022-12-11 16:02:55 +0100

    Day 10 - refactorings

--- src/main/kotlin/be/fgov/sfpd/kata/aoc22/day10/CathodeRayTube.kt
+++ src/main/kotlin/be/fgov/sfpd/kata/aoc22/day10/CathodeRayTube.kt
@@ -5,15 +5,11 @@ import be.fgov.sfpd.kata.aoc22.mapLines
 val interestingCycles = listOf(20, 60, 100, 140, 180, 220)
 fun part1(input: String) = VideoSystem().executeProgram(input.toCRTCommands()).history
         .filter { it.cycle in interestingCycles }
-        .sumOf { it.cycle * it.registerX }
+        .sumOf { it.signalStrength }
 
-fun part2(input: String) =
-        VideoSystem().executeProgram(input.toCRTCommands())
-                .lineImage(40).mapIndexed { index, cycleImage ->
-                    if ((index + 1) % 40 == 0) cycleImage + "\n" else cycleImage
-                }.joinToString("").trimIndent()
+fun part2(input: String) = VideoSystem().executeProgram(input.toCRTCommands()).image(rolloverAt = 40)
 
-class VideoSystem() {
+private class VideoSystem {
     val history = mutableListOf(CPUState())
 
     fun executeProgram(command: List<CRTCommand>): VideoSystem {
@@ -21,18 +17,11 @@ class VideoSystem() {
         return this
     }
 
-    private fun execute(command: CRTCommand) {
-        when (command.name) {
-            "noop" -> history.add(cpuStateAfter(command))
-            "addx" -> {
-                history.add(cpuStateAfter(CRTCommand("noop", 0)))
-                history.add(cpuStateAfter(command))
-            }
-            else -> error("Invalid Command: ${command.name}")
-        }
-    }
+    fun image(rolloverAt: Int) = lineImage(rolloverAt).mapIndexed { index, cycleImage ->
+        if ((index + 1) % rolloverAt == 0) cycleImage + "\n" else cycleImage
+    }.joinToString("").trimIndent()
 
-    fun lineImage(rolloverAt: Int) =
+    private fun lineImage(rolloverAt: Int) =
             history.dropLast(1).map { cpuState ->
                 when (cpuState.registerX) {
                     in sprite(cpuState.cycle - 1, rolloverAt) -> "#"
@@ -40,16 +29,26 @@ class VideoSystem() {
                 }
             }
 
+    private fun execute(command: CRTCommand) {
+        when (command.name) {
+            "noop" -> history.add(cpuStateAfter(command))
+            "addx" -> {
+                history.add(cpuStateAfter(CRTCommand("noop")))
+                history.add(cpuStateAfter(command))
+            }
+
+            else -> error("Invalid Command: ${command.name}")
+        }
+    }
+
     private fun cpuStateAfter(command: CRTCommand) = history.last().afterExecuting(command)
 
-    private fun sprite(cycle: Int, rolloverAt: Int): List<Int> {
-        val rollovers = (cycle / rolloverAt) * rolloverAt
-        return listOf((if (cycle - 1 >= 0) cycle - 1 else 0), cycle, cycle + 1).map { it - rollovers }
-    }
+    private fun sprite(cycle: Int, rolloverAt: Int) = (cycle - 1..cycle + 1).map { it - (cycle / rolloverAt) * rolloverAt }
 }
 
 data class CPUState(val cycle: Int = 1, val registerX: Int = 1) {
-    fun afterExecuting(crtCommand: CRTCommand) = this.copy(cycle = cycle + 1, registerX = registerX + crtCommand.value)
+    val signalStrength = cycle * registerX
+    fun afterExecuting(crtCommand: CRTCommand) = copy(cycle = cycle + 1, registerX = registerX + crtCommand.value)
 }
 
 fun String.toCRTCommands() = mapLines { line ->
@@ -58,6 +57,5 @@ fun String.toCRTCommands() = mapLines { line ->
         else -> line.split(" ").let { (command, value) -> CRTCommand(command, value.toInt()) }
     }
 }
-
 
 data class CRTCommand(val name: String, val value: Int = 0)
