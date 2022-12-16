0644424a35927d9dc356cc5ea272e1d2e2e24f99
sukes13 <sir_jelle_wouters@hotmail.com>
2022-12-16 23:22:26 +0100

    Day 13 - refactorings

--- src/main/kotlin/be/fgov/sfpd/kata/aoc22/day13/DistressSignal.kt
+++ src/main/kotlin/be/fgov/sfpd/kata/aoc22/day13/DistressSignal.kt
@@ -1,7 +1,7 @@
 package be.fgov.sfpd.kata.aoc22.day13
 
+import be.fgov.sfpd.kata.aoc22.day13.PacketValue.ArrayPacketValue
 import be.fgov.sfpd.kata.aoc22.day13.PacketValue.IntPacketValue
-import be.fgov.sfpd.kata.aoc22.day13.PacketValue.ListPacketValue
 import be.fgov.sfpd.kata.aoc22.mapLines
 import be.fgov.sfpd.kata.aoc22.splitOnEmptyLine
 
@@ -16,16 +16,17 @@ fun part1(input: String): Int {
 
 fun part2(input: String): Int {
     val dividerPackets = listOf("[[2]]".toPacket(), "[[6]]".toPacket())
-    val packets = (input.replace("\r\n\r\n", "\r\n")).mapLines { it.toPacket() } + dividerPackets
+    val allPackets = (input.replace("\r\n\r\n", "\r\n")).mapLines { it.toPacket() } + dividerPackets
 
-    return packets.sortedWith{ packet1, packet2 -> packet2.comesBefore(packet1)   }.mapIndexed { index, packet ->
+    return allPackets.sortedWith { packet1, packet2 -> packet2.comesBefore(packet1) }.mapIndexed { index, packet ->
         if (packet in dividerPackets) index + 1 else 1
     }.reduce { a, b -> a * b }
 }
 
 sealed class PacketValue {
-    data class ListPacketValue(val values: List<PacketValue> = listOf()) : PacketValue() {
-        fun comesBefore(other: ListPacketValue): Int {
+
+    data class ArrayPacketValue(val values: List<PacketValue> = listOf()) : PacketValue() {
+        fun comesBefore(other: ArrayPacketValue): Int {
             val longestListSize = if (values.size <= other.values.size) other.values.size else values.size
             var match = 0
             var index = 0
@@ -36,26 +37,23 @@ sealed class PacketValue {
                     packet1 == null -> {
                         match = 1.also { println("-> in order because: left ran out first") }
                     }
-
                     packet2 == null -> {
                         match = (-1).also { println("-> NOT in order because: right ran out first") }
                     }
-
-                    packet1 is ListPacketValue && packet2 is ListPacketValue -> {
+                    packet1 is ArrayPacketValue && packet2 is ArrayPacketValue -> {
                         match += packet1.comesBefore(packet2)
                     }
-
-                    packet1 is IntPacketValue && packet2 is ListPacketValue -> {
-                        match += ListPacketValue(listOf(packet1)).comesBefore(packet2)
+                    packet1 is IntPacketValue && packet2 is ArrayPacketValue -> {
+                        match += ArrayPacketValue(listOf(packet1)).comesBefore(packet2)
                     }
-
-                    packet2 is IntPacketValue && packet1 is ListPacketValue -> {
-                        match += packet1.comesBefore(ListPacketValue(listOf(packet2)))
+                    packet2 is IntPacketValue && packet1 is ArrayPacketValue -> {
+                        match += packet1.comesBefore(ArrayPacketValue(listOf(packet2)))
                     }
-
                     packet1 is IntPacketValue && packet2 is IntPacketValue -> {
-                        if (packet1 < packet2) match = 1.also { println("-> in order because: $packet1 < $packet2") }
-                        if (packet1 > packet2) match = (-1).also { println("-> NOT in order because: $packet1 > $packet2") }
+                        when {
+                            packet1 < packet2 -> match = 1.also { println("-> in order because: $packet1 < $packet2") }
+                            packet1 > packet2 -> match = (-1).also { println("-> NOT in order because: $packet1 > $packet2") }
+                        }
                     }
                 }
                 index += 1
@@ -75,22 +73,22 @@ sealed class PacketValue {
 
 fun String.toPacket() = drop(1).dropLast(1).toPacketValue()
 
-fun String.toPacketValue(): ListPacketValue {
+fun String.toPacketValue(): ArrayPacketValue {
     var stringToCheck = this
-    var listPacketValue = ListPacketValue()
+    var arrayPacketValue = ArrayPacketValue()
 
     while (stringToCheck.isNotEmpty()) {
         when {
             stringToCheck.first() == '[' -> {
-                val entireList = stringToCheck.findNextListContent()
-                listPacketValue = listPacketValue.copy(values = listPacketValue.values.toMutableList() + entireList.toPacketValue())
+                val entireList = stringToCheck.findNextArrayContent()
+                arrayPacketValue = arrayPacketValue.copy(values = arrayPacketValue.values.toMutableList() + entireList.toPacketValue())
                 stringToCheck = stringToCheck.drop(entireList.length + 1)
             }
 
             stringToCheck.first().isDigit() -> {
                 val allValues = stringToCheck.substringBefore('[').substringBefore(']')
                 val packetValuesToAdd = allValues.toIntPackages()
-                listPacketValue = listPacketValue.copy(values = listPacketValue.values.toMutableList() + packetValuesToAdd)
+                arrayPacketValue = arrayPacketValue.copy(values = arrayPacketValue.values.toMutableList() + packetValuesToAdd)
                 stringToCheck = stringToCheck.drop(allValues.length)
             }
 
@@ -100,24 +98,24 @@ fun String.toPacketValue(): ListPacketValue {
         }
     }
 
-    return listPacketValue
+    return arrayPacketValue
 }
+
+private fun String.findNextArrayContent(): String {
+    var content = substringBefore(']')
+    (0..6).forEach { _ ->
+        content = this.towardsWholeArray(content)
+    }
+    return content.drop(1)
+}
+
+private fun String.towardsWholeArray(listContent: String): String {
+    val deptInList = listContent.count { it == '[' }
+    return positionOfCloserFor(deptInList)?.let { correctedCloser ->
+        this.take(correctedCloser)
+    } ?: listContent.drop(1)
+}
+
+private fun String.positionOfCloserFor(dept: Int) = Regex("]").findAll(this).map { it.range.first }.toList().getOrNull(dept - 1)
 
 private fun String.toIntPackages() = split(",").filter { it != "" }.map { it.toInt() }.map { IntPacketValue(it) }
-
-private fun String.findNextListContent(): String {
-    var listContent = substringBefore(']')
-    (0..6).forEach { _ ->
-        listContent = run {
-            val deptInList = listContent.count { it == '[' }
-            this.positionOfCloserFor(deptInList)?.let { correctedCloser ->
-                this.take(correctedCloser)
-            } ?: listContent.drop(1)
-        }
-    }
-    return listContent.drop(1)
-}
-
-private fun String.positionOfCloserFor(dept: Int) =
-        Regex("]").findAll(this).map { it.range.first }.toList().getOrNull(dept - 1)
-
