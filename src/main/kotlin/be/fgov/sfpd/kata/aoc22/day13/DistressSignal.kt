6bf573a3f2ef2799d45d82218505a368fe1556a5
sukes13 <sir_jelle_wouters@hotmail.com>
2022-12-16 01:18:33 +0100

    Day 13 - almost working part 1

--- src/main/kotlin/be/fgov/sfpd/kata/aoc22/day13/DistressSignal.kt
+++ src/main/kotlin/be/fgov/sfpd/kata/aoc22/day13/DistressSignal.kt
@@ -2,12 +2,22 @@ package be.fgov.sfpd.kata.aoc22.day13
 
 import be.fgov.sfpd.kata.aoc22.day13.PacketValue.IntPacketValue
 import be.fgov.sfpd.kata.aoc22.day13.PacketValue.ListPacketValue
+import be.fgov.sfpd.kata.aoc22.mapLines
+import be.fgov.sfpd.kata.aoc22.splitOnEmptyLine
 
-fun part1(input: String) = ""
+fun part1(input: String): Int {
+    val checks = input.splitOnEmptyLine().map { pairOfPackets ->
+        val packets = pairOfPackets.mapLines { it.toPacket() }.windowed(2).map { (first, second) -> first to second }.first()
+        packets.first.comesBefore(packets.second)
+
+    }
+
+    return checks.mapIndexed { index, check -> if (check) index else 0 }.sum()
+}
 
 fun part2(input: String) = ""
 
-fun String.toPacket(): ListPacketValue = drop(1).dropLast(1).toPacketValue()
+fun String.toPacket() = drop(1).dropLast(1).toPacketValue()
 
 fun String.toPacketValue(): ListPacketValue {
     var stringToCheck = this
@@ -57,12 +67,27 @@ private fun String.positionOfCloserFor(dept: Int) =
 
 sealed class PacketValue {
     data class ListPacketValue(val values: List<PacketValue> = listOf()) : PacketValue() {
-
+        fun comesBefore(other: ListPacketValue): Boolean {
+            return values.zip(other.values).all { (packet1, packet2) ->
+                if (packet1 is IntPacketValue && packet2 is IntPacketValue) {
+                    packet1.comesBefore(packet2) && values.size <= other.values.size//TODO: issue here if right runs out first
+                } else if (packet1 is IntPacketValue) {
+                    ListPacketValue(listOf(packet1)).comesBefore(packet2 as ListPacketValue) && packet2.values.isNotEmpty()
+                } else if (packet2 is IntPacketValue) {
+                    (packet1 as ListPacketValue).comesBefore(ListPacketValue(listOf(packet2))) && packet1.values.isNotEmpty()
+                } else if (packet1 is ListPacketValue && packet2 is ListPacketValue) {
+                    (packet1).comesBefore(packet2) && packet1.values.size <= packet2.values.size
+                } else {
+                    error("")
+                }
+            }
+        }
     }
 
     data class IntPacketValue(val value: Int) : PacketValue() {
+        fun comesBefore(other: IntPacketValue): Boolean {
+            return value <= other.value
+        }
 
     }
-
-
 }
--- src/test/kotlin/be/fgov/sfpd/kata/aoc22/day13/SolveDay13Test.kt
+++ src/test/kotlin/be/fgov/sfpd/kata/aoc22/day13/SolveDay13Test.kt
@@ -6,17 +6,17 @@ import org.junit.jupiter.api.Test
 
 class SolveDay13Test {
 
-//    @Test
-//    fun `example input part 1`() {
-//        val input = readFile("day13/exampleInput.txt")
-//        assertThat(part1(input)).isEqualTo(31)
-//    }
-//
-//    @Test
-//    fun `actual input part 1`() {
-//        val input = readFile("day13/input.txt")
-//        assertThat(part1(input)).isEqualTo(528)
-//    }
+    @Test
+    fun `example input part 1`() {
+        val input = readFile("day13/exampleInput.txt")
+        assertThat(part1(input)).isEqualTo(13)
+    }
+
+    @Test
+    fun `actual input part 1`() {
+        val input = readFile("day13/input.txt")
+        assertThat(part1(input)).isEqualTo(1333) //Too low
+    }
 //
 //    @Test
 //    fun `example input part 2`() {
