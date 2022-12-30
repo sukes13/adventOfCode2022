54ea24ab2eadf097a03d6571eaa98fca02619604
sukes13 <sir_jelle_wouters@hotmail.com>
2022-12-30 21:05:35 +0100

    Day 18 - solution part 2 - refactored dijkstra-scan from day 12 to count where all empty spots in a path in an enclosing cube, starting outside the droplet, have sides touching the droplet

--- src/main/kotlin/be/fgov/sfpd/kata/aoc22/day18/BoilingBoulders.kt
+++ src/main/kotlin/be/fgov/sfpd/kata/aoc22/day18/BoilingBoulders.kt
@@ -2,17 +2,18 @@ package be.fgov.sfpd.kata.aoc22.day18
 
 import be.fgov.sfpd.kata.aoc22.flatMapLines
 
-fun part1(input: String) = input.toDroplets().countFreeSides()
 
-fun part2(input: String) = input.toDroplets().countOuterSides()
+fun part1(input: String) = input.toDroplet().countFreeSides()
 
-private fun List<Droplet>.countFreeSides() = sumOf { droplet -> (6 - droplet.sides().count { it in this }) }
+fun part2(input: String) = input.toDroplet().countOuterSides()
 
-private fun List<Droplet>.countOuterSides() = enclosingCube().dijkstraScanForOuterSidesOf(this)
+private fun List<LavaCube>.countFreeSides() = sumOf { lavaCube -> 6 - lavaCube.sides().count { it in this } }
 
-private fun List<Droplet>.dijkstraScanForOuterSidesOf(droplets: List<Droplet>): Int {
-    val queue = associate { droplet ->
-        if (droplet == first()) droplet to 0 else droplet to Int.MAX_VALUE
+private fun List<LavaCube>.countOuterSides() = enclosingCube().dijkstraScanForOuterSidesOf(this)
+
+private fun List<LavaCube>.dijkstraScanForOuterSidesOf(lavaCubes: List<LavaCube>): Int {
+    val queue = associate { lavaCube ->
+        if (lavaCube == first()) lavaCube to 0 else lavaCube to Int.MAX_VALUE
     }.toMutableMap()
 
     var outerSides = 0
@@ -27,34 +28,34 @@ private fun List<Droplet>.dijkstraScanForOuterSidesOf(droplets: List<Droplet>): 
         current.key.accessibleNeighbours(queue)
                 .forEach { neighbour ->
                     when (neighbour) {
-                        in droplets -> outerSides += 1
+                        in lavaCubes -> outerSides += 1
                         else -> queue[neighbour] = current.value + 1
                     }
                 }
     }
 
-    error("No droplets found...")
+    error("No lava-cubes found...")
 }
 
-private fun List<Droplet>.enclosingCube() =
+private fun List<LavaCube>.enclosingCube() =
         (minOf { it.x } - 1..maxOf { it.x } + 1).flatMap { x ->
             (minOf { it.y } - 1..maxOf { it.y } + 1).flatMap { y ->
                 (minOf { it.z } - 1..maxOf { it.z } + 1).mapNotNull { z ->
-                    Droplet(x, y, z)
+                    LavaCube(x, y, z)
                 }
             }
         }
 
-data class Droplet(val x: Int, val y: Int, val z: Int) {
+data class LavaCube(val x: Int, val y: Int, val z: Int) {
     fun sides() = listOf(
             this.copy(x = x + 1), this.copy(x = x - 1),
             this.copy(y = y + 1), this.copy(y = y - 1),
             this.copy(z = z + 1), this.copy(z = z - 1)
     )
 
-    fun accessibleNeighbours(queue: MutableMap<Droplet, Int>) = queue.keys.filter { it in sides() }
+    fun accessibleNeighbours(dijkstraQueue: MutableMap<LavaCube, Int>) = dijkstraQueue.keys.filter { it in sides() }
 }
 
-private fun String.toDroplets(): List<Droplet> = flatMapLines {
-    it.split(",").map(String::toInt).windowed(3).map { (x, y, z) -> Droplet(x, y, z) }
+private fun String.toDroplet(): List<LavaCube> = flatMapLines {
+    it.split(",").map(String::toInt).windowed(3).map { (x, y, z) -> LavaCube(x, y, z) }
 }
