ac824af6eb397ea8412699c33d4b037d29806d02
sukes13 <sir_jelle_wouters@hotmail.com>
2023-01-07 19:20:52 +0100

    Day 21 - refactorings

--- src/main/kotlin/be/fgov/sfpd/kata/aoc22/day21/MonkeyMath.kt
+++ src/main/kotlin/be/fgov/sfpd/kata/aoc22/day21/MonkeyMath.kt
@@ -13,15 +13,34 @@ fun List<MathMonkey>.findYellOf(targetMonkey: String): Long {
     val operationMonkeys = filterIsInstance<OperationMonkey>().toMutableList()
 
     while(targetMonkey !in valueMonkeys.keys){
-        val yellingMonkeys = operationMonkeys.filter { monkey ->
-            monkey.nameMonkey1 in valueMonkeys.keys && monkey.nameMonkey2 in valueMonkeys.keys
-        }
-        yellingMonkeys.forEach {
-            valueMonkeys[it.name] = it.operator.operation(valueMonkeys[it.nameMonkey1]!!,valueMonkeys[it.nameMonkey2]!!)
+        operationMonkeys.filter { it.valuesFound(valueMonkeys.keys)}.forEach {
+            valueMonkeys[it.name] = it.operator.executeFor(valueMonkeys[it.nameMonkey1]!!,valueMonkeys[it.nameMonkey2]!!)
             operationMonkeys.remove(it)
         }
     }
+
     return valueMonkeys.filterKeys { it == targetMonkey }.values.single()
+}
+
+
+sealed class MathMonkey {
+    abstract val name: String
+
+    data class ValueMonkey(override val name: String, val value: Long) : MathMonkey()
+    data class OperationMonkey(override val name: String, val nameMonkey1: String, val nameMonkey2: String, val operator: MonkeyOperator) : MathMonkey(){
+        fun valuesFound(valueMonkeys: MutableSet<String>) = nameMonkey1 in valueMonkeys && nameMonkey2 in valueMonkeys
+    }
+}
+
+enum class MonkeyOperator(val sign: String, val executeFor: (Long, Long) -> Long) {
+    PLUS("+", { a, b -> a + b }),
+    MINUS("-", { a, b -> a - b }),
+    MULTIPLY("*", { a, b -> a * b }),
+    DIVIDE("/", { a, b -> a / b });
+
+    companion object {
+        fun bySign(sign: String) = MonkeyOperator.values().find { it.sign == sign } ?: error("Invalid sign found: $sign")
+    }
 }
 
 internal fun String.toMathMonkeys(): List<MathMonkey> = mapLines { line ->
@@ -32,23 +51,5 @@ internal fun String.toMathMonkeys(): List<MathMonkey> = mapLines { line ->
             val (nameMonkey1, sign, nameMonkey2) = leftOver.trimIndent().split(" ").take(3)
             OperationMonkey(name, nameMonkey1, nameMonkey2, MonkeyOperator.bySign(sign))
         }
-    }
-}
-
-sealed class MathMonkey {
-    abstract val name: String
-
-    data class ValueMonkey(override val name: String, val value: Long) : MathMonkey()
-    data class OperationMonkey(override val name: String, val nameMonkey1: String, val nameMonkey2: String, val operator: MonkeyOperator) : MathMonkey()
-}
-
-enum class MonkeyOperator(val sign: String, val operation: (Long, Long) -> Long) {
-    PLUS("+", { a, b -> a + b }),
-    MINUS("-", { a, b -> a - b }),
-    MULTIPLY("*", { a, b -> a * b }),
-    DIVIDE("/", { a, b -> a / b });
-
-    companion object {
-        fun bySign(sign: String) = MonkeyOperator.values().find { it.sign == sign } ?: error("Invalid sign found: $sign")
     }
 }
