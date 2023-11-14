292634cbd788406f7d5058c9c401e25287200213
Jelle Wouters <88056283+sukes13@users.noreply.github.com>
2023-11-14 15:27:59 +0100

    Update Utils

--- src/main/kotlin/be/fgov/sfpd/kata/aoc22/Utils.kt
+++ src/main/kotlin/be/fgov/sfpd/kata/aoc22/Utils.kt
@@ -3,12 +3,13 @@ package be.fgov.sfpd.kata.aoc22
 fun readFile(fileName: String): String =
     {}::class.java.classLoader.getResourceAsStream(fileName)?.reader()?.readText() ?: error("Could not load $fileName")
 
-fun <T> String.mapLines(variant: (String) -> T) = this.lines().map(variant)
-fun <T> String.flatMapLines(variant: (String) -> Iterable<T>) = this.lines().flatMap(variant)
+fun <T> String.mapLines(variant: (String) -> T) = lines().map(variant)
 
-fun String.filterLines(variant: (String) -> Boolean) = this.lines().filter(variant)
+fun <T> String.flatMapLines(variant: (String) -> Iterable<T>) = lines().flatMap(variant)
 
-fun String.splitOnEmptyLine() = this.split("\r\n\r\n")
+fun String.filterLines(variant: (String) -> Boolean) = lines().filter(variant)
+
+fun String.splitOnEmptyLine() = split("\r\n\r\n")
 
 /**
  *@receiver Returns set of all elements shared in all sets
